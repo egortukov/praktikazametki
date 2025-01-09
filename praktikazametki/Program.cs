2f3f2ac7b19446bbc67b92416621b9c9e46fcfe6
egortukov <egor1305991@gmail.com>
2025-01-09 15:19:15 +0900

    zadanie1

--- praktikazametki/Program.cs
+++ praktikazametki/Program.cs
@@ -9,16 +9,16 @@ class Program
 
 public class Notes
 {
-    public string Title { get; private set; }
-    public string Description { get; private set; }
-    public DateTime Date { get; private set; }
-    public bool State { get; private set; }
-    public Users Owner { get; private set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public bool State { get; set; }
+    public Users Owner { get; set; }
 }
 
 public class Users
 {
-    public string Name { get; private set; }
-    public DateTime RegistrationDate { get; private set; }
-    public List<Notes> Notes { get; private set; }
+    public string Name { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public List<Notes> Notes { get; set; }
 }
