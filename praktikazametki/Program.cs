bb3c36170149361e7a5a026183bbd6ae341aeadc
egortukov <egor1305991@gmail.com>
2025-01-09 15:01:49 +0900

    Zadanie1

---
+++ praktikazametki/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace praktikazametki;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+public class Notes
+{
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public DateTime Date { get; private set; }
+    public bool State { get; private set; }
+    public Users Owner { get; private set; }
+}
+
+public class Users
+{
+    public string Name { get; private set; }
+    public DateTime RegistrationDate { get; private set; }
+    public List<Notes> Notes { get; private set; }
+}
